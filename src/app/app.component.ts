951f3453cbc5f69fe319c940a06c2ee05f366dda
Cesar Illanes <cesarill99@gmail.com>
2020-08-07 15:56:50 -0400

    InputOutputSolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,18 @@ export class AppComponent implements OnInit{
   title = 'curso-angular';
 
   ngOnInit(){
-    const array = from([1,2,3,4,5,6]);    
-    array.subscribe(s => console.log('item: ',s));
+    // const array = from([1, 2, 3, 4, 5, 6]);
+    // array.subscribe(s => console.log('item: ',s));
 
-    const aux = fromEvent(document, 'mousemove');
-    aux.subscribe((s:any) => console.log('Event: ', s.clientX + ', '+ s.clientY));
+    // const aux = fromEvent(document, 'mousemove');
+    // aux.subscribe((s:any) => console.log('Event: ', s.clientX + ', '+ s.clientY));
   }
 
   saveClickChild(event){
     console.log('Event Child: ', event);
   }
 
-  click(event){
+  print(event){
     console.log('Persona: ', event);
   }
 }
