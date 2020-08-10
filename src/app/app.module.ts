9fd24a40f73d9e481bce428c96eb34b38c85bd24
Cesar Illanes <cesarill99@gmail.com>
2020-08-10 15:56:24 -0400

    Component Life Cicle example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { ComponentInputOutputComponent } from './component-input-output/component-input-output.component';
+import { CiclosDeVidaComponent } from './ciclos-de-vida/ciclos-de-vida.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    ComponentInputOutputComponent
+    ComponentInputOutputComponent,
+    CiclosDeVidaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
