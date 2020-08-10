da05bd3a3df5412baa02f7d760b4a37c4a0e2643
Cesar Illanes <cesarill99@gmail.com>
2020-08-10 16:30:51 -0400

    ngFor example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import {from, fromEvent} from 'rxjs';
 export class AppComponent implements OnInit{
   title = 'curso-angular';
   sw = true;
+  persons = ['Manuel', 'Pepe', 'Persona Genérica 1', 'Persona Genérica 2'];
 
   ngOnInit(){
     // const array = from([1, 2, 3, 4, 5, 6]);
