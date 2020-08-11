b0e76aa32a3088b6e0100b5af0b82a96902379b5
Cesar Illanes <cesarill99@gmail.com>
2020-08-11 15:45:09 -0400

    ngClass examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit{
   persons = ['Manuel', 'Pepe', 'Persona Genérica 1', 'Persona Genérica 2'];
   switch = 2;
   styleColor = 'red';
+  class = true;
 
   ngOnInit(){
     // const array = from([1, 2, 3, 4, 5, 6]);
