6df78906dd9cb77752b3b014173965baae5ea8a8
Cesar Illanes <cesarill99@gmail.com>
2020-08-30 19:22:47 -0400

    Homework 08-26-2020 1. Card with Angular Material 2. Login and Pages 3. *ngIf with else

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products = [
+    {
+      name: 'Salteña',
+      description: 'Salteña de carne',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerID: 1,
+      price: 20
+    },
+    {
+      name: 'Salteña',
+      description: 'Salteña de pollo',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerID: 1,
+      price: 20
+    },
+    {
+      name: 'Salteña',
+      description: 'Salteña de fricasé',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerID: 1,
+      price: 20
+    },
+    {
+      name: 'Salteña',
+      description: 'Salteña de pizza',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerID: 1,
+      price: 20
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
